8246168af7f65c7be74268c78a7e11767b8e5acd
hema <hemasatyadevi@gmail.com>
2023-06-05 00:33:16 +0400

    commit3

--- 08-functions.sh
+++ 08-functions.sh
@@ -1,1 +1,43 @@
 #!/bin/bash
+
+USERID=$(id -u)
+DATE$(date+%F-%T)
+LOG_FILE="$DATE.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+#checking the user is root or not
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root user access"
+    exit12
+fi    
+
+#this is a generic function, we need to pass arguments
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ....$R FAILED $N"
+        exit 12
+    else
+        echo -e "$2 ...$G SUCCESS $N"
+    fi    
+}
+
+yum install git -y &>>$LOG_FILE
+
+VALIDATE $? "GIT INSTALLATION"
+
+yum install vim -y &>>$LOG_FILE
+
+VALIDATE $? "VIM Installation"
+
+yum install wget -y &>>$LOG_FILE
+
+VALIDATE $? "WGET Installation"
+
+yum install net-tools -y &>>$LOG_FILE
+
+VALIDATE $? "NET-TOOLS Installation"
--- 09-loops.sh
+++ 09-loops.sh
@@ -1,1 +1,42 @@
 #!/bin/bash
+
+USERID=$(id -u)
+DATE=$(date +"%F-%H-%M-%S")
+LOG_FILE="$DATE.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+#check user is root or not
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root user access"
+    exit 1
+fi
+
+#this is a generic function, we need to pass arguments
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+for PACKAGE IN $@   #sample input: git vim net-tools wget
+do 
+  
+  yum -q list installed $PACKAGE &>>/dev/null
+  if [ $? -ne 0 ]
+  then 
+      echo -e "$PACKAGE ... $R not installed $N"
+      yum install $PACKAGE -y &>>$LOG_FILE
+      VALIDATE $? "$PACKAGE installation"
+      else
+          echo -e "$PACKAGE ....$Y installed already $N"
+   fi
+done
+      
+
